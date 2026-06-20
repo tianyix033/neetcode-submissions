9746a3825b1c690d74bdfb775fa6fa4999abdd5a
Tianyi Xu <tianyi.x033@gmail.com>
2026-06-20 15:56:14 -0400

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        partition = []
+        cache = {}
+        def isPalindrome(left, right):
+            if (left, right) in cache:
+                return cache[(left, right)]
+            while left < right:
+                if s[left] != s[right]:
+                    cache[(left, right)] = False
+                    return False
+                left += 1
+                right -= 1
+            cache[(left, right)] = True
+            return True
+
+        def helper(i):
+            if i >= len(s):
+                res.append(partition.copy())
+                return
+            for j in range(i, len(s)):
+                if isPalindrome(i, j):
+                    partition.append(s[i : j + 1])
+                    helper(j + 1)
+                    partition.pop()
+
+        helper(0)
+        return res
+
+
+    
+        
