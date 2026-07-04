0613849969611871b39855b35f7c1f4776f82ded
Tianyi Xu <tianyi.x033@gmail.com>
2026-07-03 20:37:35 -0400

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.py
@@ -0,0 +1,42 @@
+class WordDictionary:
+    class Node:
+        def __init__(self):
+            self.children = {}
+            self.is_eow = False
+
+    def __init__(self):
+        self.root = self.Node()
+
+    def addWord(self, word: str) -> None:
+        curr_node = self.root
+        word_idx = 0
+        while word_idx < len(word) and word[word_idx] in curr_node.children:
+            curr_node = curr_node.children[word[word_idx]]
+            if word_idx == len(word) - 1:
+                curr_node.is_eow = True
+            word_idx += 1
+            
+        while word_idx < len(word):
+            child = self.Node()
+            curr_node.children[word[word_idx]] = child
+            curr_node = child
+            if word_idx == len(word) - 1:
+                curr_node.is_eow = True
+            word_idx += 1
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(word_idx, curr_node):
+            if word_idx == len(word):
+                return curr_node.is_eow
+            res = False
+            c = word[word_idx]
+            if c == '.':
+                for child_node in curr_node.children.values():
+                    res = res or dfs(word_idx + 1, child_node)
+            else:   # c is a specific letter
+                if c in curr_node.children:
+                    res = dfs(word_idx + 1, curr_node.children[c])
+            return res
+
+        return dfs(0, self.root)
