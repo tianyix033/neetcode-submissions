968e6db5983ebc37b277d98a449d6155582f747e
Tianyi Xu <tianyi.x033@gmail.com>
2026-07-14 20:27:27 -0400

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        step_end = 0
+        next_step_end = 0
+        curr_steps = 0
+        i = 0
+        while i < len(nums):
+            while i < len(nums) and i <= step_end:
+                next_step_end = max(next_step_end, i + nums[i])
+                i += 1
+            step_end = next_step_end
+            if i < len(nums):
+                curr_steps += 1
+        return curr_steps
+
