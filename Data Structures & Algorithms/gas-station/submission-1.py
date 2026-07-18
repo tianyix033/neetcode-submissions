a425589206f74c124325ac0c1296103c08e3700b
Tianyi Xu <tianyi.x033@gmail.com>
2026-07-18 17:09:01 -0400

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        seen = [False] * n
+        max_seen = -1
+
+        def traverse(curr, start, gas_amount):
+            seen[curr] = True
+            new_gas_amount = gas_amount + gas[curr] - cost[curr]
+            if new_gas_amount < 0:
+                return False
+            else:
+                if (curr + 1) % n == start:
+                    return True
+                return traverse((curr + 1) % n, start, new_gas_amount)
+
+        for i in range(n):
+            if i > max_seen:
+                curr = i
+                gas_amount = 0
+                while True:
+                    max_seen = max(max_seen, curr)
+                    gas_amount += gas[curr] - cost[curr]
+                    if gas_amount < 0:
+                        break
+                    elif (curr + 1) % n == i:
+                            return i
+                    curr = (curr + 1) % n
+
+        return -1
