5a056a4814e9989ec3150ea078af1aad58065e62
Tianyi Xu <tianyi.x033@gmail.com>
2026-09-07 11:17:58 -0400

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        queens = []
+        cols = [False] * n
+        diags_1 = defaultdict(bool)
+        diags_2 = defaultdict(bool)
+        res = []
+        def helper(r):
+            if len(queens) == n:
+                res.append(queens.copy())
+                return
+            for c in range(n):
+                if (not cols[c]) and (not diags_1[r - c]) and (not diags_2[r + c]):
+                    queens.append((r, c))
+                    cols[c] = True
+                    diags_1[r - c] = True
+                    diags_2[r + c] = True
+                    helper(r + 1)
+                    cols[c] = False
+                    diags_1[r - c] = False
+                    diags_2[r + c] = False
+                    queens.pop()
+        
+        helper(0)
+        # print(res)
+        ret = []
+        for queens in res:
+            grid = [["."] * n for _ in range(n)]
+            for queen_r, queen_c in queens:
+                grid[queen_r][queen_c] = "Q"
+            grid = ["".join(row) for row in grid]
+            ret.append(grid.copy())
+        return ret
